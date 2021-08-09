ea5bc7ac9574a6eab31a5ccde879c21bd1ba03d1
Charles Desiderio <cjdesi@gmail.com>
2021-08-08 22:22:05 -0500

    Build login and register forms

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState } from 'react'
+
+import Login from "./components/Login"
+import Register from './components/Register'
+
 const App = () => {
+
+  const [newUserRegister, setNewUserRegister] = useState(false)
+  const [toggleButtonValue, setToggleButtonValue] = useState('Register')
+
+  const toggleNewUserRegister = () => {
+    setNewUserRegister(!newUserRegister)
+    if (newUserRegister) {
+      setToggleButtonValue('Register')
+    } else {
+      setToggleButtonValue('Back to Login')
+    }
+  }
+
   return (
     <div>
-      Hello kitty
+      {newUserRegister ? <Register /> : <Login />}
+      <button onClick={toggleNewUserRegister}>{ toggleButtonValue }</button>
+      
     </div>
   )
 }
