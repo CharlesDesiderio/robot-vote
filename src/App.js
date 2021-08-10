9d01ec5473c052e0c8e6cfe5759b1dd7aa220016
Charles Desiderio <cjdesi@gmail.com>
2021-08-09 22:20:37 -0500

    Registration component complete

--- src/App.js
+++ src/App.js
@@ -1,84 +1,127 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import UserContext from "./contexts/UserContext";
-import Authentication from "./components/Authentication"
+import UserContext from './contexts/UserContext';
+import Authentication from './components/Authentication';
 import Main from './components/Main';
 
 const App = () => {
-
   const initialUserData = {
     loggedIn: false,
     id: '',
     name: '',
-    email: ''
-  }
+    email: '',
+  };
 
-  const [userData, setUserData] = useState(initialUserData)
-  const [errorMessage, setErrorMessage] = useState('')
+  const [userData, setUserData] = useState(initialUserData);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const updateUserData = (data) => {
     let newUserData = {
       loggedIn: true,
       id: data.id,
       name: data.name,
-      email: data.email
-    }
-    setUserData(newUserData)
-  }
+      email: data.email,
+    };
+    setUserData(newUserData);
+  };
 
   const attemptLogin = (email, password) => {
+    setErrorMessage('')
     let loginData = {
       email: email,
-      password: password
-    }
+      password: password,
+    };
 
     fetch('https://mondo-robot-art-api.herokuapp.com/auth/session', {
       method: 'POST',
       headers: {
         'x-robot-art-api-key': '346ee7ddde4bb72637e20fe9eff91306',
-        'Content-Type': 'application/json'
-      }, body: JSON.stringify(loginData)
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginData),
     })
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        throw new Error('Failed to login. Please check credentials and try again.')
-      }
-    })
-    .then(token => {
-
-      fetch('https://mondo-robot-art-api.herokuapp.com/auth/session', {
-        method: 'GET',
-        headers: {
-          'x-robot-art-api-key': '346ee7ddde4bb72637e20fe9eff91306',
-          'Authorization': `Bearer ${token.token}`,
-          'Content-Type': 'application/json'
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(
+            'Failed to login. Please check credentials and try again.'
+          );
         }
       })
-      .then(response => response.json())
-      .then(data => {
+      .then((token) => {
+        fetch('https://mondo-robot-art-api.herokuapp.com/auth/session', {
+          method: 'GET',
+          headers: {
+            'x-robot-art-api-key': '346ee7ddde4bb72637e20fe9eff91306',
+            Authorization: `Bearer ${token.token}`,
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            let newUserData = {
+              loggedIn: true,
+              id: data.id,
+              name: data.name,
+              email: data.email,
+            };
+            setUserData(newUserData);
+          });
+      })
+      .catch((error) => {
+        setErrorMessage(
+          'Failed to login. Please check credentials and try again.'
+        );
+      });
+  };
+
+  const attemptRegister = (fullName, email, password) => {
+    setErrorMessage('')
+    let newUser = {
+      name: fullName,
+      email: email,
+      password: password,
+    };
+
+    fetch('https://mondo-robot-art-api.herokuapp.com/auth/register', {
+      method: 'POST',
+      headers: {
+        'x-robot-art-api-key': '346ee7ddde4bb72637e20fe9eff91306',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Invalid User.');
+        }
+      })
+      .then((data) => {
         let newUserData = {
           loggedIn: true,
           id: data.id,
           name: data.name,
-          email: data.email
-        }
-        setUserData(newUserData)
+          email: data.email,
+        };
+        setUserData(newUserData);
       })
-    })
-    .catch((error) => {
-      setErrorMessage('Failed to login. Please check credentials and try again.')
-    })
-  }
+      .catch((error) => {
+        setErrorMessage('Invalid User.');
+      });
+  };
 
   return (
     <div>
-      <UserContext.Provider value={{userData, updateUserData, attemptLogin, errorMessage}}>
-        { userData.loggedIn ? <Main /> : <Authentication />}
+      <UserContext.Provider
+        value={{ userData, updateUserData, attemptLogin, attemptRegister, errorMessage, setErrorMessage }}
+      >
+        {userData.loggedIn ? <Main /> : <Authentication />}
       </UserContext.Provider>
     </div>
-  )
-}
+  );
+};
 
 export default App;
