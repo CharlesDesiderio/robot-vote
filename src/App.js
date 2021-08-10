5f04d3bbdf9a27dab4eb7015812adc1d88a33f81
Charles Desiderio <cjdesi@gmail.com>
2021-08-09 20:42:54 -0500

    Can successfully log in

--- src/App.js
+++ src/App.js
@@ -1,27 +1,75 @@
 import { useState } from 'react'
 
-import Login from "./components/Login"
-import Register from './components/Register'
+import UserContext from "./contexts/UserContext";
+import Authentication from "./components/Authentication"
+import Main from './components/Main';
 
 const App = () => {
 
-  const [newUserRegister, setNewUserRegister] = useState(false)
-  const [toggleButtonValue, setToggleButtonValue] = useState('Register')
+  const initialUserData = {
+    loggedIn: false,
+    id: '',
+    name: '',
+    email: ''
+  }
 
-  const toggleNewUserRegister = () => {
-    setNewUserRegister(!newUserRegister)
-    if (newUserRegister) {
-      setToggleButtonValue('Register')
-    } else {
-      setToggleButtonValue('Back to Login')
+  const [userData, setUserData] = useState(initialUserData)
+
+  const updateUserData = (data) => {
+    let newUserData = {
+      loggedIn: true,
+      id: data.id,
+      name: data.name,
+      email: data.email
     }
+    setUserData(newUserData)
+  }
+
+  const attemptLogin = (email, password) => {
+    let loginData = {
+      email: email,
+      password: password
+    }
+
+    console.log(loginData)
+
+    fetch('https://mondo-robot-art-api.herokuapp.com/auth/session', {
+      method: 'POST',
+      headers: {
+        'x-robot-art-api-key': '346ee7ddde4bb72637e20fe9eff91306',
+        'Content-Type': 'application/json'
+      }, body: JSON.stringify(loginData)
+    })
+    .then(response => response.json())
+    .then(token => {
+      console.log(token)
+
+      fetch('https://mondo-robot-art-api.herokuapp.com/auth/session', {
+        method: 'GET',
+        headers: {
+          'x-robot-art-api-key': '346ee7ddde4bb72637e20fe9eff91306',
+          'Authorization': `Bearer ${token.token}`,
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        let newUserData = {
+          loggedIn: true,
+          id: data.id,
+          name: data.name,
+          email: data.email
+        }
+        setUserData(newUserData)
+      })
+    })
   }
 
   return (
     <div>
-      {newUserRegister ? <Register /> : <Login />}
-      <button onClick={toggleNewUserRegister}>{ toggleButtonValue }</button>
-      
+      <UserContext.Provider value={{userData, updateUserData, attemptLogin}}>
+        { userData.loggedIn ? <Main /> : <Authentication />}
+      </UserContext.Provider>
     </div>
   )
 }
